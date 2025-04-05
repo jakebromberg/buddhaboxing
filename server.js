fa761938368488a7314d87402c10851b0e603470
Jake Bromberg <jake.bromberg@gmail.com>
2025-04-05 07:19:51 -0700

    Animated color transition while a loop is playing

--- server.js
+++ server.js
@@ -239,6 +239,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Access from other devices using: http://<your-ip-address>:${PORT}`);
 });
